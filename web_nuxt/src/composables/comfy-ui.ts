10d18a811a82881067f528533d45e3105acd213d
yuichi bouzu ishihara <yuichi.ishihara@bouzu.design>
2024-10-10 20:09:37 +0900

    feat: comfyui の状況変数を追加し開発しやすさ改善

--- web_nuxt/src/composables/comfy-ui.ts
+++ web_nuxt/src/composables/comfy-ui.ts
@@ -12,8 +12,16 @@ declare global {
 }
 // 100ミリ秒ごとにチェック
 const CHECK_INTERVAL = 100;
+const OUTPUT_PATH_URL = `http://127.0.0.1:8188/api/view?filename={filename}&subfolder={subfolder}&type={type}`
 
+// Data ------------------
 const isInit = ref(false);
+const state = ref<'' | 'start' | 'executing' | 'executed' | 'error'>(''); // ステータス
+const queueRemaining = ref(0); // キューの残数
+const progress = ref(0); // 進捗 0 - 1
+const output = ref<string[]>([]); // 出力ファイルパス
+
+// ComfyUI ------------------
 let app: any = null;
 let api: any = null;
 
@@ -21,8 +29,12 @@ export const useComfyUI = () => {
 	return {
 		checkInit,
 		isInit,
+		state,
+		queueRemaining,
+		progress,
+		output,
 		loadGraph,
-		queueStart,
+		queuePrompt,
 	};
 };
 
@@ -44,6 +56,35 @@ const checkInit = async () => {
 					isInit.value = true;
 					app = window.comfyAPI?.app?.app;
 					api = window.comfyAPI?.api?.api;
+
+					/**	
+					 * 進捗状況を取得する
+					 * @see https://docs.comfy.org/essentials/comms_messages#built-in-message-types
+					 */
+					api.addEventListener('execution_start', () => {
+						state.value = 'start'
+					})
+					api.addEventListener('execution_error', () => {
+						state.value = 'error'
+					})
+					api.addEventListener('execution_interrupted', () => {
+						state.value = 'error'
+					})
+					api.addEventListener('execution_cached', () => {
+						state.value = 'start'
+					})
+					api.addEventListener('executing', () => {
+						state.value = 'executing'
+					})
+					api.addEventListener('executed', (evt) => {
+						state.value = 'executed'
+						output.value.push(evt.detail.output.images.map((image: { filename: string, subfolder: string, type: string }) => {
+							return OUTPUT_PATH_URL.replace('{filename}', image.filename).replace('{subfolder}', image.subfolder).replace('{type}', image.type);
+						})[0]);
+					})
+					api.addEventListener('progress', (evt: any) => { progress.value = evt.detail.value / evt.detail.max })
+					api.addEventListener('status', (evt: any) => { queueRemaining.value = evt.detail.exec_info.queue_remaining })
+
 					flag = true
 					resolve();
 				}
@@ -60,6 +101,7 @@ const checkInit = async () => {
 
 /**
  * グラフを読み込む
+ * @param {string} json - グラフJSON
  */
 const loadGraph = (json: string) => {
 	if (!isInit.value || !app) return;
@@ -68,8 +110,17 @@ const loadGraph = (json: string) => {
 
 /**
  * キューを開始
+ * @param {string} json - グラフJSON
+ * @param {number} batchCount - 実行回数
  */
-const queueStart = () => {
+const queuePrompt = async ({ json = '', batchCount = 1 }: { json?: string, batchCount?: number } = {}) => {
 	if (!isInit.value || !app) return;
-	app.queuePrompt();
+	state.value = '';
+	queueRemaining.value = 0;
+	progress.value = 0;
+	output.value = [];
+	if (json !== '') {
+		loadGraph(json);
+	}
+	app.queuePrompt(0, batchCount);
 }
--- web_nuxt/src/pages/dev/comfy-ui/index.vue
+++ web_nuxt/src/pages/dev/comfy-ui/index.vue
@@ -1,6 +1,12 @@
 <template>
 	<div class="pageDevComfyUIIndex">
 		<Column justify="stretch" gap="8">
+			<Typography title2>
+				<Input v-model="count" name="devComfyUIBatchCount" type="number" />
+			</Typography>
+			<Typography>
+				Status: {{ state }}, Queue: {{ queueRemaining }}, Progress: {{ progress }}
+			</Typography>
 			<Button @click="toggleComfyUI">
 				Show Toggle ComfyUI
 			</Button>
@@ -8,16 +14,25 @@
 				<Button @click="loadGraph">
 					Load Graph
 				</Button>
-				<Button @click="queueStart">
+				<Button @click="queuePrompt">
 					Queue Start
 				</Button>
 			</Row>
+			<Ratio v-for="path in output" :key="path">
+				<Image :src="path" />
+			</Ratio>
 		</Column>
 	</div>
 </template>
 
 <script setup lang="ts">
 import test001 from '~/../workflows/test001.json';
+
+// Composables ------------------
+const { state, queueRemaining, progress, output } = useComfyUI();
+
+// Data ------------------
+const count = ref<string>('1');
 
 // Methods ------------------
 const toggleComfyUI = () => {
@@ -28,8 +43,9 @@ const loadGraph = () => {
 	useComfyUI().loadGraph(JSON.stringify(test001));
 }
 
-const queueStart = () => {
-	useComfyUI().queueStart();
+const queuePrompt = () => {
+	// ToDo: 進捗状況や出力ファイルのパスを取得する
+	useComfyUI().queuePrompt({ json: JSON.stringify(test001), batchCount: parseInt(count.value) });
 }
 </script>
 
