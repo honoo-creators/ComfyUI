e0652f88fc6df225a911ec9bf0f76cbc492fd7d8
yuichi ishihara <yuichi.ishihara@honoo.tokyo>
2024-12-13 14:01:42 +0900

    feat: Update v0.1.0-alpha.2

---
+++ web/_nuxt/BcNIklcX.js
@@ -0,0 +1,1 @@
+import{d as o,g as a,f as s,h as r,w as n,q as c,H as p,z as _}from"./WHSOQWA4.js";const i={class:"prompt"},u=o({__name:"Prompt",props:{text:{type:String,default:"",required:!0}},setup(t){return(l,d)=>{const e=_;return a(),s("div",i,[r(e,{caption3:"",bold:"",unselectable:"",color:"light"},{default:n(()=>[c(p(t.text),1)]),_:1})])}}});export{u as _};
--- web/_nuxt/DZV68eSE.js
+++
@@ -1,1 +0,0 @@
-import{ax as a,a6 as e,n as t,c as s}from"./5NVV-yna.js";const i=a(()=>{if(!e().isInit.value)return t(s().COMFYUI_INIT)});export{i as default};
