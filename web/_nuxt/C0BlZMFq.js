24acf1a585006c96d63517ca28508a39933aa627
yuichi ishihara <yuichi.ishihara@honoo.tokyo>
2024-12-13 15:50:42 +0900

    test: Add custom look style func

---
+++ web/_nuxt/C0BlZMFq.js
@@ -0,0 +1,1 @@
+import{aO as a,a5 as e,n as t,c as s}from"./DTE7kaSZ.js";const i=a(()=>{if(!e().isInit.value)return t(s().COMFYUI_INIT)});export{i as default};
--- web/_nuxt/CMNY9vxf.js
+++
@@ -1,1 +0,0 @@
-import{aN as a,a5 as e,n as t,c as s}from"./CVjm8Pq9.js";const i=a(()=>{if(!e().isInit.value)return t(s().COMFYUI_INIT)});export{i as default};
