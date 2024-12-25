a343482043196ba6da63f4d8fc61d70b39fce21e
yuichi ishihara <yuichi.ishihara@honoo.tokyo>
2024-12-25 17:49:30 +0900

    fix: Upscale func

--- web/_nuxt/CgxxKsYy.js
+++
@@ -1,1 +0,0 @@
-import{aT as t,b as s,aU as o,a0 as r,s as u,C as i}from"./CZv2J2H9.js";const d=t(async()=>{let e,a;if(!s().loggedIn.value)return[e,a]=o(()=>r().open({name:"loginRequired"})),await e,a(),u({statusCode:Number(i.AuthorizationRequired),statusMessage:"ToDo: Navigate to Top Page"})});export{d as default};
