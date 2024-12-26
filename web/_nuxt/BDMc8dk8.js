a16adf0d5e088772db46ce40f202b7596c42cf8d
yuichi ishihara <yuichi.ishihara@honoo.tokyo>
2024-12-26 11:20:33 +0900

    fix: Resolve issue where generated image is added to output page during upscale

---
+++ web/_nuxt/BDMc8dk8.js
@@ -0,0 +1,1 @@
+import{aT as t,b as s,aU as o,a0 as r,s as u,C as i}from"./DvrT95oI.js";const d=t(async()=>{let e,a;if(!s().loggedIn.value)return[e,a]=o(()=>r().open({name:"loginRequired"})),await e,a(),u({statusCode:Number(i.AuthorizationRequired),statusMessage:"ToDo: Navigate to Top Page"})});export{d as default};
