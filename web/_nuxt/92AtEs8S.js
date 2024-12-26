9b5ec25758deeba2fd2c072877e4959fc4e728d7
yuichi ishihara <yuichi.ishihara@honoo.tokyo>
2024-12-26 18:50:12 +0900

    feat: Refine input prompt. Resolve generate not stop when leave output page.

---
+++ web/_nuxt/92AtEs8S.js
@@ -0,0 +1,1 @@
+import{aT as a,a5 as e,n as t,c as s}from"./Cloa7yoR.js";const i=a(()=>{if(!e().isInit.value)return t(s().COMFYUI_INIT)});export{i as default};
--- web/_nuxt/BAWZB3IN.js
+++
@@ -1,1 +0,0 @@
-import{aT as a,a5 as e,n as t,c as s}from"./CPnOD-C8.js";const i=a(()=>{if(!e().isInit.value)return t(s().COMFYUI_INIT)});export{i as default};
