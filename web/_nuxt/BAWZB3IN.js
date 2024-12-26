511cf65a9574e921d003b1d4671dbbc3609a2d3e
yuichi ishihara <yuichi.ishihara@honoo.tokyo>
2024-12-26 17:28:27 +0900

    feat: Refine UI viewable during pause

---
+++ web/_nuxt/BAWZB3IN.js
@@ -0,0 +1,1 @@
+import{aT as a,a5 as e,n as t,c as s}from"./CPnOD-C8.js";const i=a(()=>{if(!e().isInit.value)return t(s().COMFYUI_INIT)});export{i as default};
--- web/_nuxt/DwA1ABGp.js
+++
@@ -1,1 +0,0 @@
-import{aT as a,a5 as e,n as t,c as s}from"./IrKfp_ne.js";const i=a(()=>{if(!e().isInit.value)return t(s().COMFYUI_INIT)});export{i as default};
