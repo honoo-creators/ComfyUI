755e5523c6aa3dda750cc77cca7b59fdbb4c35a4
yuichi ishihara <yuichi.ishihara@honoo.tokyo>
2025-01-10 17:28:38 +0900

    test: pause processing time

--- web/_nuxt/C7mmED41.js
+++
@@ -1,1 +0,0 @@
-import{aV as t,b as s,aW as o,a0 as r,s as u,C as i}from"./CODnliH5.js";const d=t(async()=>{let e,a;if(!s().loggedIn.value)return[e,a]=o(()=>r().open({name:"loginRequired"})),await e,a(),u({statusCode:Number(i.AuthorizationRequired),statusMessage:"ToDo: Navigate to Top Page"})});export{d as default};
