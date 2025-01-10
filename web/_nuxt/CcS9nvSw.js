8da3c8437834949f5e1e7dd26eee781b2bd4f8cc
yuichi ishihara <yuichi.ishihara@honoo.tokyo>
2025-01-10 20:40:48 +0900

    fix: Ensure bottom border of image is rounded

---
+++ web/_nuxt/CcS9nvSw.js
@@ -0,0 +1,1 @@
+import{aV as a,a5 as e,n as t,c as s}from"./DN2TbqHU.js";const i=a(()=>{if(!e().isInit.value)return t(s().COMFYUI_INIT)});export{i as default};
--- web/_nuxt/DjOWCYkX.js
+++
@@ -1,1 +0,0 @@
-import{d as n,f as t,i as o,a3 as _}from"./Cc3goawm.js";const r=n({__name:"nuxt",setup(a){return(c,s)=>{const e=_;return t(),o(e)}}});export{r as default};
