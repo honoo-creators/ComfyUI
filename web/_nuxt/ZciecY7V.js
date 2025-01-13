4cfacabf54a915ce525a441de3ec296e31f6bd73
yuichi ishihara <yuichi.ishihara@honoo.tokyo>
2025-01-13 20:31:40 +0900

    test: add processing modal

--- web/_nuxt/ZciecY7V.js
+++
@@ -1,1 +0,0 @@
-import{aW as u,c as o,au as r,aL as p,n as v}from"./CgPfrX_a.js";const d=u(s=>{if(s.path!==o().GENERATE_LOOKSTYLE){const{lookStyle:l,base64:n,promptScene:e,promptMood:a,promptLighting:t}=r(p());if(!l.value&&!n.value&&e.value&&!e.value.length&&a.value&&!a.value.length&&t.value&&!t.value.length)return v(o().dashboard())}});export{d as default};
