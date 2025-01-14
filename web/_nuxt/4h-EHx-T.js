0b30fad9ee7f85c29fdf7772b3956d24eb66eb8c
yuichi ishihara <yuichi.ishihara@honoo.tokyo>
2025-01-14 17:29:01 +0900

    test: starting text show before mounted

--- web/_nuxt/4h-EHx-T.js
+++
@@ -1,1 +0,0 @@
-import{a_ as a,a5 as e,n as t,c as s}from"./aZAmJsgB.js";const i=a(()=>{if(!e().isInit.value)return t(s().COMFYUI_INIT)});export{i as default};
--- web/_nuxt/xchHqGuz.js
+++
@@ -1,1 +0,0 @@
-import{a_ as t,b as s,a$ as o,a0 as r,s as u,C as i}from"./aZAmJsgB.js";const d=t(async()=>{let e,a;if(!s().loggedIn.value)return[e,a]=o(()=>r().open({name:"loginRequired"})),await e,a(),u({statusCode:Number(i.AuthorizationRequired),statusMessage:"ToDo: Navigate to Top Page"})});export{d as default};
