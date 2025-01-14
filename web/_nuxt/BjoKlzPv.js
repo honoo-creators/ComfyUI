b609a4d4ed9cae1c985da36f5ac5b5ea01697057
yuichi ishihara <yuichi.ishihara@honoo.tokyo>
2025-01-14 11:46:49 +0900

    test: add init func before execute

--- web/_nuxt/BjoKlzPv.js
+++
@@ -1,1 +0,0 @@
-import{d as n,f as t,i as o,a3 as _}from"./4mCWTx_e.js";const r=n({__name:"nuxt",setup(a){return(c,s)=>{const e=_;return t(),o(e)}}});export{r as default};
---
+++ web/_nuxt/CKyP9Qk-.js
@@ -0,0 +1,1 @@
+import{d as n,f as t,i as o,a3 as _}from"./Y71BdUJA.js";const r=n({__name:"nuxt",setup(a){return(c,s)=>{const e=_;return t(),o(e)}}});export{r as default};
