ef833362bdf98d5c052fa67fce2f5d767f4c727d
yuichi ishihara <yuichi.ishihara@honoo.tokyo>
2025-01-14 13:05:53 +0900

    test: add node type when change seed

--- web/_nuxt/DVKq-WfJ.js
+++
@@ -1,1 +0,0 @@
-import{d as n,f as t,i as o,a3 as _}from"./yeVF_5_d.js";const r=n({__name:"nuxt",setup(a){return(c,s)=>{const e=_;return t(),o(e)}}});export{r as default};
---
+++ web/_nuxt/u2r6Ms-v.js
@@ -0,0 +1,3 @@
+import{d as o,f as c,i,w as a,z as e,ak as n,U as _,V as p}from"./DPpA0FVg.js";const l=o({__name:"upscale-ui",setup(m){return(r,d)=>{const s=_,t=p;return c(),i(t,{class:"pageDevElementsUsageType"},{default:a(()=>[e(s,{gap:"40"},{default:a(()=>[e(n,{icon:"tv",name:"テレビ・配信",description:`フルHD (1920x1080ピクセル) 
+アスペクト比 16:9`}),e(n,{icon:"tv",name:"テレビ・配信",description:`フルHD (1920x1080ピクセル) 
+アスペクト比 16:9`,processing:""}),e(n,{icon:"tv",name:"テレビ・配信",description:"フルHD (1920x1080ピクセル) アスペクト比 16:9",disabled:""})]),_:1})]),_:1})}}});export{l as default};
