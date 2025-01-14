a5e14c12b93eeccf1bfcb0877460dc97c9df2d8c
yuichi ishihara <yuichi.ishihara@honoo.tokyo>
2025-01-14 11:02:27 +0900

    test: Add feature to change aspect ratio

--- web/_nuxt/D3Bl6HIv.js
+++
@@ -1,1 +0,0 @@
-import{d as n,f as t,i as o,a3 as _}from"./CU11xXy2.js";const r=n({__name:"nuxt",setup(a){return(c,s)=>{const e=_;return t(),o(e)}}});export{r as default};
---
+++ web/_nuxt/Z8RgAZK8.js
@@ -0,0 +1,1 @@
+import{aY as a,a5 as e,n as t,c as s}from"./96gK2lUC.js";const i=a(()=>{if(!e().isInit.value)return t(s().COMFYUI_INIT)});export{i as default};
